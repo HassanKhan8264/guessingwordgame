7ee9be91910c13bc2c3e53241c9e5d4a4fac81a8
Hassan Khan <hf610061@gmail.com>
2022-06-15 21:00:53 -0700

    new

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// var alphabets = ["karachi","islamabad","lahore","hyderabad"]
+// var x = Math.floor(Math.random() * alphabets.length);
+// var random = alphabets[x]
+function save() {
+    var input = document.getElementById("user");
+    var value = input.value;
+    if(value === "Hassan"){
+        var score = 0; 
+        alert("Bingo Correct"+" "+"Score = 1");
+        if (window.confirm("Go to Next Level")) {
+              window.location.href = "./leveltwo.html"
+            }
+    }else if(value !== "Hassan"){
+        document.getElementById("sample2").innerText = "Wrong answer"
+        alert("You've Enter a wrong word")
+    }else{
+        document.getElementById("sample2").innerText = ""
+        
+    }
+   
+}
+
