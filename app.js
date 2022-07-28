ffca3bab636698e26ab95cd32caac90c68aa1d05
Hassan Khan <hf610061@gmail.com>
2022-07-29 03:52:27 +0500

    new

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // click sound
 const audio = new Audio ('./button-20.mp3')
-const button = document.querySelector('butn')
+const button = document.getElementById('button')
 
 button.addEventListener('click', function() {
   audio.play()
 })
 // click sound end
+
+
 
